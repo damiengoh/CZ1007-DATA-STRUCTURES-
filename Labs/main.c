12e4a9114443db293c602bbd8deea90537eb62c2
damiengoh <49312564+damiengoh@users.noreply.github.com>
2020-03-09 16:19:41 +0800

    Add files via upload

---
+++ Labs/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int row, col, height;
+    int num = 0;
+    printf("Enter the height: \n");
+    scanf("%d", &height);
+    printf("Pattern: \n");
+
+    for(row=0;row<height;row++){
+        for(col=0;col<row+1;col++){
+            printf("%d", num+1);
+
+        }
+        num++;
+        if(num ==3)
+            num = 0;
+        printf("\n");
+    }
+    return 0;
+}
